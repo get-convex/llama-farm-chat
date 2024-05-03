f925b2348db0e004b8cb8578379f0880c4158866
Ian Macartney <ianhmacartney@gmail.com>
2024-05-03 12:31:38 -0700

    remove unused ui

--- src/components/typography/link.tsx
+++
@@ -1,7 +0,0 @@
-import { se } from "@/lib/utils";
-import { AnchorHTMLAttributes } from "react";
-
-export const Link = se<
-  HTMLAnchorElement,
-  AnchorHTMLAttributes<HTMLAnchorElement>
->("a", "font-medium text-primary");
